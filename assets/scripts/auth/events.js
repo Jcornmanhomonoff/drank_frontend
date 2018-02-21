13854f141aace8501780d78231e407fc50de366d
jcornmanhomonoff <jessica.cornman-homonoff@generalassemb.ly>
2018-02-21 11:52:35 -0500

    Refactor code
    - Separate event handlers from event listener

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,48 +1,37 @@
 'use strict'
 
 const getFormFields = require('../../../lib/get-form-fields')
-const app = require('../app-data')
-const authApi = require('./api')
+const appData = require('../app-data')
+const api = require('./api')
 const ui = require('./ui')
 const showDrankTemplate = require('./templates/drank-content.handlebars')
 const drinkApi = require('./drinkApi')
 const store = require('../store')
 
-const addHandlers = () => {
-  $('#sign-up').on('submit', function (event) {
-    let data = getFormFields(this)
-    console.log("sign-up success")
-    event.preventDefault()
-    authApi.signUp(ui.success, ui.failure, data)
-  })
-  $('#sign-in').on('submit', function (event) {
-    let data = getFormFields(this)
-    console.log("sign-in success")
-    event.preventDefault()
-    authApi.signIn(ui.signInSuccess, ui.failure, data)
-  })
-  $('#sign-out').on('click', function (event) {
-    console.log("sign-out success")
-    event.preventDefault()
-    authApi.signOut(ui.signOutSuccess, ui.failure)
-  })
-  $('#change-password').on('submit', function (event){
-    let data = getFormFields(this)
-    event.preventDefault()
-    authApi.changePassword(ui.changePasswordSuccess, ui.failure, data)
-  })
-  $('#create-drink').on('submit', function (event){
-    event.preventDefault()
-    let data = getFormFields(this)
-    console.log(data)
-    data.drinks.user_id = app.id
-    drinkApi.newDrink(ui.newDrinkSuccess, ui.failure, data)
-    authApi.newIngredient(ui.newIngredientSuccess, ui.failure, data)
-  })
-  $('.content').on('click', '.drink', onShowDrank)
-  $('.content').on('click', '.edit-drink', onOpenEditDrank)
-  $('.content').on('submit', '.update-drink', onEditDrank)
-}
+const onSignUp = function (event) {
+  let data = getFormFields(this)
+  event.preventDefault()
+  api.signUp(ui.success, ui.failure, data)
+})
+
+const onSignIn = function (event) {
+  let data = getFormFields(this)
+  console.log("sign-in success")
+  event.preventDefault()
+  api.signIn(ui.signInSuccess, ui.failure, data)
+})
+
+const onSignOut = function (event) {
+  console.log("sign-out success")
+  event.preventDefault()
+  api.signOut(ui.signOutSuccess, ui.failure)
+})
+
+const onChangePw = function (event){
+  let data = getFormFields(this)
+  event.preventDefault()
+  api.changePassword(ui.changePasswordSuccess, ui.failure, data)
+})
 
 const onShowDrank = function (event) {
     event.preventDefault()
@@ -56,6 +45,15 @@ const onShowDrank = function (event) {
     const showDrankHtml = showDrankTemplate({ drank })
     $('.drink-content').html(showDrankHtml)
 }
+
+const onCreateDrank = function (event){
+  event.preventDefault()
+  let data = getFormFields(this)
+  console.log(data)
+  data.drinks.user_id = appData.id
+  drinkApi.newDrink(ui.newDrinkSuccess, ui.failure, data)
+  api.newIngredient(ui.newIngredientSuccess, ui.failure, data)
+})
 
 let drinkId = ''
 
@@ -72,6 +70,17 @@ const onEditDrank = function (event) {
   event.preventDefault()
   const drinkName = $(this).siblings().val()
   drinkApi.editDrank(ui.editDrankSuccess, ui.failure, drinkName, drinkId)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePw)
+  $('#create-drink').on('submit', onCreateDrank)
+  $('#sign-out').on('click', onSignOut)
+  $('.content').on('click', '.drink', onShowDrank)
+  $('.content').on('click', '.edit-drink', onOpenEditDrank)
+  $('.content').on('submit', '.update-drink', onEditDrank)
 }
 
 module.exports = {
